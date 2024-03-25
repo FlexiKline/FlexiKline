10e9143c03fdd5403c28a2c76ac31dbd188616b1
zhaopanpan <zhaopanpan@newhuotech.com>
2024-03-25 19:07:42 +0800

    feat: move candle v2

--- lib/src/core/data_source.dart
+++ lib/src/core/data_source.dart
@@ -46,21 +46,27 @@ mixin DataSourceBinding
   /// 此变量仅限于当前蜡烛数小于一屏绘制时
   /// 注: 滑动/缩放都主要通过调整其值.
   /// maxPaintWidth - _paintDxOffset 就是X轴起始的绘制点.
-  double _startDxOffset = 0;
-  double get startDxOffset => _startDxOffset;
-  set startDxOffset(double val) {
-    _startDxOffset = math.max(val, 0.0);
-  }
+  // double _startDxOffset = 0;
+  // double get startDxOffset => _startDxOffset;
+  // set startDxOffset(double val) {
+  //   _startDxOffset = math.max(val, 0.0);
+  // }
 
-  /// 代表当前绘制区域外部X轴的移动偏移量. 必须大于0.
-  /// 此变量仅限于当前蜡烛数大于一屏绘制时
-  /// 注: 滑动/缩放都主要通过调整其值.
-  /// maxPaintWidth - _paintDxOffset 就是X轴起始的绘制点.
+  /// 代表当前绘制区域相对于startIndex的偏移量.
+  /// 取值范围 [-canvasWidth, maxPaintWidth - minPaintWidthInCanvas]
+  /// 1. 当为零时(默认) : 说明首根蜡烛在Canvas最右边展示.
+  /// 2. 当为负数时     : 代表首根蜡烛到Canvas右部的距离
+  /// 3. 当为正数时     : 说明有部分蜡烛已向右移出Canvas.
+  ///     移出数量 = (paintDxOffset / candleActualWidth).floor()
+  ///     移出Offset = paintDxOffset % candleActualWidth;
   double _paintDxOffset = 0;
   @override
   double get paintDxOffset => _paintDxOffset;
   set paintDxOffset(double val) {
-    _paintDxOffset = math.max(0.0, val);
+    _paintDxOffset = math.min(
+      math.max(-canvasWidth, val),
+      maxPaintWidth - minPaintWidthInCanvas,
+    );
   }
 
   /// 计算绘制蜡烛图的起始数组索引下标和绘制偏移量
@@ -69,26 +75,27 @@ mixin DataSourceBinding
     logd(
       'calculateCandleIndexAndPaintOffset needless! $maxPaintWidth <= $canvasWidth',
     );
-    if (isCanDrawAllCandlesInCanvas) {
-      // 不足一屏
-      final startIndex = (startDxOffset / candleActualWidth).floor();
-      final dxOffset = startDxOffset % candleActualWidth;
-      curCandleData.ensureIndexAndOffset(
-        startIndex,
-        dxOffset,
-        maxCandleCount: maxCandleCount,
-      );
-      curCandleData.calculateMaxmin();
-    } else {
-      final startIndex = (paintDxOffset / candleActualWidth).floor();
-      final dxOffset = paintDxOffset % candleActualWidth;
-      curCandleData.ensureIndexAndOffset(
-        startIndex,
-        dxOffset,
-        maxCandleCount: maxCandleCount,
-      );
-      curCandleData.calculateMaxmin();
-    }
+    // if (isCanDrawAllCandlesInCanvas) {
+    //   // // 不足一屏
+    //   // final startIndex = (startDxOffset / candleActualWidth).floor();
+    //   // final dxOffset = startDxOffset % candleActualWidth;
+    //   // curCandleData.ensureIndexAndOffset(
+    //   //   startIndex,
+    //   //   dxOffset,
+    //   //   maxCandleCount: maxCandleCount,
+    //   // );
+    //   // curCandleData.calculateMaxmin();
+    // } else {
+    //   //
+    // }
+    final startIndex = (paintDxOffset / candleActualWidth).floor();
+    final dxOffset = paintDxOffset % candleActualWidth;
+    curCandleData.ensureIndexAndOffset(
+      startIndex,
+      dxOffset,
+      maxCandleCount: maxCandleCount,
+    );
+    curCandleData.calculateMaxmin();
   }
 
   @override
@@ -129,28 +136,29 @@ mixin DataSourceBinding
     super.handleScale(data);
     if (!data.moved) return;
     // logd('${DateTime.now()} move $paintDxOffset dxDelta:${data.dxDelta}');
-    if (isCanDrawAllCandlesInCanvas) {
-      paintDxOffset = 0;
-      // 不足一屏, 调整绘制区域内部的偏移量:data.offset
-      final newStartDxOffset = (startDxOffset + data.dxDelta).clamp(
-        canvasWidth - minPaintWidthInCanvas,
-        0.0,
-      );
-      if (newStartDxOffset != startDxOffset) {
-        startDxOffset = newStartDxOffset;
-        markRepaintCandle();
-      }
-    } else {
-      startDxOffset = 0;
-      // 满足一屏, 调整绘制区域外部的偏移量:paintDxOffset
-      final newDxOffset = (paintDxOffset + data.dxDelta).clamp(
-        0.0,
-        maxPaintWidth - minPaintWidthInCanvas,
-      );
-      if (newDxOffset != paintDxOffset) {
-        paintDxOffset = newDxOffset;
-        markRepaintCandle();
-      }
+    // if (isCanDrawAllCandlesInCanvas) {
+    //   // paintDxOffset = 0;
+    //   // // 不足一屏, 调整绘制区域内部的偏移量:data.offset
+    //   // final newStartDxOffset = (startDxOffset + data.dxDelta).clamp(
+    //   //   canvasWidth - minPaintWidthInCanvas,
+    //   //   0.0,
+    //   // );
+    //   // if (newStartDxOffset != startDxOffset) {
+    //   //   startDxOffset = newStartDxOffset;
+    //   //   markRepaintCandle();
+    //   // }
+    // } else {
+    //   // startDxOffset = 0;
+    //   // 满足一屏, 调整绘制区域外部的偏移量:paintDxOffset
+    // }
+
+    final newDxOffset = math.min(
+      math.max(-canvasWidth, paintDxOffset + data.dxDelta),
+      maxPaintWidth - minPaintWidthInCanvas, // ?
+    );
+    if (newDxOffset != paintDxOffset) {
+      paintDxOffset = newDxOffset;
+      markRepaintCandle();
     }
   }
 
