0babde792256efbb982da28696374758f631a3c2
zhaopanpan <kangqiao610@gmail.com>
2024-05-07 23:09:12 +0800

    feat: 添加测试用例; 分析关键计算性能

---
+++ test/chart_paint_test.dart
@@ -0,0 +1,82 @@
+// Copyright 2024 Andy.Zhao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:ui';
+
+import 'package:flexi_kline/flexi_kline.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'mock.dart';
+
+void main() {
+  final stopwatch = Stopwatch();
+  final controller = FlexiKlineController();
+  late SinglePaintObjectIndicator maIndicator;
+  final canvas = Canvas(PictureRecorder());
+  const mainSize = Size(400, 300);
+  int start = 50;
+  int end = 100;
+
+  setUpAll(() {
+    debugPrint('setUpAll');
+    final list = getCandleModelList();
+    controller.setKlineData(
+      CandleReq(
+        instId: 'BTC-USDT',
+        bar: TimeBar.m15.bar,
+        precision: 4,
+      ),
+      list,
+    );
+
+    controller.setMainSize(mainSize);
+
+    controller.addIndicatorInMain(maKey);
+    controller.addIndicatorInMain(emaKey);
+    controller.addIndicatorInMain(bollKey);
+    controller.addIndicatorInMain(volumeKey);
+
+    controller.curKlineData.ensureStartAndEndIndex(start, end);
+
+    controller.mainIndicator.ensurePaintObject(controller);
+
+    maIndicator = controller.mainIndicator.children.firstWhere(
+      (child) => child.key == maKey,
+    );
+  });
+
+  setUp(() {
+    debugPrint('setUp');
+    stopwatch.reset();
+    stopwatch.start();
+  });
+
+  tearDown(() {
+    stopwatch.stop();
+    debugPrint('tearDown spent:${stopwatch.elapsedMilliseconds}');
+  });
+
+  group('paint-Ma', () {
+    test('test-MA-preprocess', () {
+      debugPrint('test-MA-preprocess');
+      controller.curKlineData.preprocess(maIndicator, start: start, end: end);
+    });
+    test('test-MA-Paint', () async {
+      debugPrint('test-MA-Paint');
+      maIndicator.paintObject?.doInitData(start: 0, end: 100);
+      maIndicator.paintObject?.doPaintChart(canvas, mainSize);
+    });
+  });
+}
---
+++ test/data_use_decimal_test.dart
@@ -0,0 +1,111 @@
+// Copyright 2024 Andy.Zhao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:math' as math;
+
+import 'package:decimal/decimal.dart';
+import 'package:flexi_kline/flexi_kline.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'mock.dart';
+
+void main() {
+  final stopwatch = Stopwatch();
+  late List<CandleModel> list;
+  late List<MaParam> calcParams;
+
+  setUpAll(() {
+    list = getCandleModelList();
+    calcParams = const [
+      MaParam(label: 'MA7', count: 7, color: Color(0xFF946F9A)),
+      MaParam(label: 'MA30', count: 30, color: Color(0xFFF1BF32))
+    ];
+  });
+
+  setUp(() {
+    stopwatch.reset();
+    stopwatch.start();
+  });
+  tearDown(() {
+    stopwatch.stop();
+    debugPrint('tearDown spent:${stopwatch.elapsedMicroseconds}');
+  });
+  test('test-use-Decimal', () {
+    debugPrint('test-use-Decimal');
+    calcuAndCacheMa(list, calcParams, 0, list.length);
+    final minmax = calcuMaMinmax(list, calcParams, 50, 100);
+    debugPrint('minmax:$minmax');
+  });
+}
+
+void calcuAndCacheMa(
+  List<CandleModel> list,
+  List<MaParam> calcParams,
+  int start,
+  int end,
+) {
+  if (list.isEmpty || calcParams.isEmpty) return;
+  int len = list.length;
+  if (start < 0 || end > len) return;
+  int? minCount;
+
+  for (var param in calcParams) {
+    minCount ??= param.count;
+    minCount = param.count < minCount ? param.count : minCount;
+  }
+
+  end = math.min(end + minCount!, len);
+
+  CandleModel m;
+  final paramLen = calcParams.length;
+  final closeSum = List.filled(paramLen, Decimal.zero, growable: false);
+  for (int i = end - 1; i >= start; i--) {
+    m = list[i];
+    m.maRets = List.filled(calcParams.length, null, growable: false);
+    for (int j = 0; j < calcParams.length; j++) {
+      closeSum[j] += m.close;
+      final count = calcParams[j].count;
+      if (i <= end - count) {
+        m.maRets![j] = closeSum[j].div(count.d);
+        closeSum[j] -= list[i + (count - 1)].close;
+      }
+    }
+  }
+}
+
+MinMax? calcuMaMinmax(
+  List<CandleModel> list,
+  List<MaParam> calcParams,
+  int start,
+  int end,
+) {
+  if (list.isEmpty || calcParams.isEmpty) return null;
+  int len = list.length;
+  if (start < 0 || end > len) return null;
+
+  if (list[start].isValidMaRets != true ||
+      list[end - 1].isValidMaRets != true) {
+    calcuAndCacheMa(list, calcParams, start, end);
+  }
+
+  MinMax? minmax;
+  CandleModel m;
+  for (int i = end - 1; i >= start; i--) {
+    m = list[i];
+    minmax ??= m.maRetsMinmax;
+    minmax?.updateMinMax(m.maRetsMinmax);
+  }
+  return minmax;
+}
